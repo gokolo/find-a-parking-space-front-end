054fee12492a296af90f1024b7016f106de3f3de
Grace Achenyo Okolo <grace.okolo15@gmail.com>
2017-12-07 19:40:21 +0200

    Setup environment

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <!-- Don't drop "q-app" class -->
+  <div id="q-app">
+    <!-- <router-view /> -->
+    <test></test>
+    
+  </div>
+</template>
+
+<script>
+/*
+ * Root component
+ */
+import Test from './components/test'
+
+export default {
+  name: 'q-app',
+  components: {
+    Test
+  }
+
+}
+</script>
+
+<style></style>
---
+++ src/components/Test.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class = "test">
+      <h1>Header 1</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'test'
+}
+</script>
+
+<style scoped>
+
+</style>
+
